8f8af6009c50b5850f056632f16c5a96ee7860e3
DomiTheKava <78154813+DomiTheKava@users.noreply.github.com>
2024-02-06 13:28:48 -0600

    Add files via upload

---
+++ operatorExercise/Calculator Without UI.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// variable defintions
+let value1 = 3
+let value2 = 2
+
+// operators
+// Add/Subtract/Multiply/Divide/Percentages
+
+func add(val1: Int, val2: Int) -> Int { return val1 + val2}
+func subtract(val1: Int, val2: Int) -> Int { return val1 - val2}
+func multiply(val1: Int, val2: Int) -> Int { return val1 * val2}
+func divide(val1: Int, val2: Int) -> Int { return val1 / val2}
+
+// Output
+
+var output = multiply(val1: value1, val2: value2)
+print(output)
+
